6f0d30d2377f4dc74472450dd7d9ffa4cf7c90f7
Tim Hall <timhall3000@gmail.com>
2015-04-24 21:43:46 -0700

    Added Tiger spec

--- spec/models/tiger_spec.rb
+++ spec/models/tiger_spec.rb
@@ -1,8 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe Tiger, type: :model do
-	describe "in the zoo" do
-		it "doesn't do anything yet"
+
+	it "is valid with an appetite" do
+		tiger = Tiger.new(appetite: 99)
+		expect(tiger).to be_valid
 	end
-	#expect(100).to eq(Tiger.appetite(default))  # passes if actual == expected
+
+	it "has a default appetite of 100" do
+		tiger = Tiger.new
+		expect(tiger.appetite).to eq 100
+	end
+
+	it "has a 'hungry?' method that returns false if appetite is 0" do
+		tiger = Tiger.new(appetite: 0)
+		expect(tiger.hungry?).to eq false
+	end
+
+	it "has a 'hungry?' method that returns true if appetite > 0" do
+		tiger = Tiger.new(appetite: 1)
+		expect(tiger.hungry?).to eq true
+	end
+
+	it "has a 'eat(zebra)' method that subtracts the zebra's weight from the tiger" do
+		tiger = Tiger.new(appetite: 500)
+		zebra = Zebra.new(weight: 200)
+		tiger.eat(zebra)
+		expect(tiger.appetite).to eq 300
+	end
+
+	it "has a 'eat(zebra)' method that destroys the zebra" do
+		tiger = Tiger.new(appetite: 500)
+		zebra = Zebra.new(weight: 200)
+		tiger.eat(zebra)
+		expect(Record.exists?(zebra)).to eq false
+	end
+
 end
