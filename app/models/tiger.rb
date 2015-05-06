21fcb54bcf8ab5748402ef7a6e5c06ff59b86dbb
Tim Hall <timhall3000@gmail.com>
2015-05-06 12:16:49 -0700

    Made tabs equal to 2 spaces throughout all Sublime Text projects

--- app/models/tiger.rb
+++ app/models/tiger.rb
@@ -1,14 +1,14 @@
 class Tiger < ActiveRecord::Base
-	validates_presence_of :appetite, message: "Appetite cannot be blank."
-	validates_numericality_of :appetite, message: "Appetite must be an integer."
+  validates_presence_of :appetite, message: "Appetite cannot be blank."
+  validates_numericality_of :appetite, message: "Appetite must be an integer."
 
-	def hungry?
-		self.appetite > 0
-	end
+  def hungry?
+    self.appetite > 0
+  end
 
-	def eat zebra
-		self.appetite -= zebra.weight
-		zebra.destroy
-	end
-	
+  def eat zebra
+    self.appetite -= zebra.weight
+    zebra.destroy
+  end
+  
 end
--- app/models/turtle.rb
+++ app/models/turtle.rb
@@ -1,13 +1,13 @@
 class Turtle < ActiveRecord::Base
-	validates_presence_of :inside, message: "Inside cannot be blank."
-	validates :inside, :inclusion => {:in => [true, false]}
+  validates_presence_of :inside, message: "Inside cannot be blank."
+  validates :inside, :inclusion => {:in => [true, false]}
 
-	def hide
-		self.inside = true
-	end
+  def hide
+    self.inside = true
+  end
 
-	def emerge
-		self.inside = false
-	end
-	
+  def emerge
+    self.inside = false
+  end
+  
 end
