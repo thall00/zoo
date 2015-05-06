3208f9efdc9ad05c984522f123a6cd72974a9a96
Tim Hall <timhall3000@gmail.com>
2015-05-06 12:13:50 -0700

    Changed tabs to spaces in giraffe.rb

--- app/models/giraffe.rb
+++ app/models/giraffe.rb
@@ -1,16 +1,17 @@
 class Giraffe < ActiveRecord::Base
-	validates_presence_of :height, message: "Height cannot be blank."
-	validates_numericality_of :height, message: "Height must be an integer."
-	# Giraffe calf is ~6 feet tall. Adult giraffes can be as tall ast 20 ft.
-	# Giraffes reach max height at age 10, and live up to 25 years.
-	def guess_age
-		if self.height < 5
-			age = 0
-		elsif self.height >= 15
-			age = rand(10..20)
-		else
-			age = self.height - 5
-		end	
-		age
-	end
+  validates_presence_of :height, message: "Height cannot be blank."
+  validates_numericality_of :height, message: "Height must be an integer."
+  # Giraffe calf is ~6 feet tall. Adult giraffes can be as tall ast 20 ft.
+  # Giraffes reach max height at age 10, and live up to 25 years.
+  def guess_age
+    if self.height < 5
+      age = 0
+    elsif self.height >= 15
+      age = rand(10..20)
+    else
+      age = self.height - 5
+    end 
+    age
+  end
 end
+
